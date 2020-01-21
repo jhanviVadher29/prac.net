6ecb8c452a31e96ddcb3d689bfbb8c7c5196df4f
jhanviVadher29 <jhanvivadher10@gmail.com>
2020-01-21 13:31:44 +0530

    commit2

---
+++ prac2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace prac2
+{
+    class Program
+    {
+        void square()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+            }
+        }
+        static void Main(string[] args)
+        {
+            square();
+            
+        }
+    }
+    
+}
+
